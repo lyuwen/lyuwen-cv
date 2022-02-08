acbb28a83ecfbc2176261d736a0fc92096de696c
Lyuwen Fu <fulvwen@gmail.com>
2022-02-09 00:19:29 +0800

    Added GitHub Action workflow

---
+++ prepare_for_publish.sh
@@ -0,0 +1,14 @@
+cat > index.html << EOF
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>CV of Lyuwen Fu</h1>
+<p>
+  <a href="LFu_CV.pdf">CV</a>
+  Created on $(date -u "+%Y-%m-%d %H:%M:%S") UTC.
+</p>
+
+</body>
+</html>
+EOF
